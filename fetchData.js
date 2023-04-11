a079f00020f9d7dd87d53f060fb3dba1526167c8
Killian Kavanagh <Killian.Kavanagh@maintel.co.uk>
2023-04-11 22:00:57 +0100

    final push

--- fetchData.js
+++ fetchData.js
@@ -14,8 +14,8 @@ fetch('/data.json')
        '<div class="flex flex-col space-y-4 p-2"></div>'      +
       `<div class="flex bg-${bgcolor} justify-center items-center rounded-xl  gap-2 p-3 ">` +
       '<img src="' + data[i].icon + '">' +
-      `<div class="flex flex-col text-${textcolor}"> ${category}  </div>` +
-      '<div class="ml-auto font-bold flex items-center gap-1">' + score + ' <div class="font-normal"> /100 </div></div>' 
+      `<div class="flex flex-col gap-1 text-lg text-${textcolor}"> ${category}  </div>` +
+      '<div class="ml-auto font-bold flex items-center gap-1">' + score + ' <div class="font-normal"> / 100 </div></div>' 
   }
 })
 .catch(function(err) {
