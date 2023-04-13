f0e0c282e931c4f2331b4d172872d403b816acd7
Killian Kavanagh <Killian.Kavanagh@maintel.co.uk>
2023-04-13 10:15:27 +0100

    added Front Menors Recommendations

--- fetchData.js
+++ fetchData.js
@@ -13,7 +13,7 @@ fetch('/data.json')
     document.getElementById("score-list").innerHTML +=   
        '<div class="flex flex-col space-y-4 p-2"></div>'      +
       `<div class="flex bg-${bgcolor} justify-center items-center rounded-xl  gap-2 p-3 ">` +
-      '<img src="' + data[i].icon + '">' +
+      '<img src="' + data[i].icon + '  " alt="icon">' +
       `<div class="flex flex-col gap-1 text-lg text-${textcolor}"> ${category}  </div>` +
       '<div class="ml-auto font-bold flex items-center gap-1">' + score + ' <div class="font-normal"> / 100 </div></div>' 
   }
